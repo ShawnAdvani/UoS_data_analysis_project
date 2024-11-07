29dacee92b7192e21dfcf3b955265941a7d3023c
Shawn Advani <54037408+ShawnAdvani@users.noreply.github.com>
2024-11-07 15:15:37 +0000

    rename markdown file

--- main.R
+++ main.R
@@ -57,7 +57,7 @@ access_df$gen_health <- recode(access_df$HUQ010,
                         '4'='fair',
                         '5'='poor'
 )
-access_df <- access_df %>% select(-HUQ010)
+# access_df <- access_df %>% select(-HUQ010)
 access_df[['HUQ010']][access_df[['HUQ010']]==9] <- 7
 access_df[['HUQ090']][access_df[['HUQ090']]==9] <- 7
 
@@ -84,14 +84,10 @@ df <- left_join(df, cond_df[c("SEQN", 'MCQ010', 'AGQ030', 'MCQ160A', 'MCQ160B',
 for (i in colnames(df)) {
   if (length(unique(df[[i]]))==3) {
     df[[i]] <- recode(df[[i]], '1' = 'Yes', '2' = 'No')
-  # } else {
-  #   df[[i]] <- recode(df[[i]], '7' = 'Missing')
   }
   print(i)
   print(unique(df[[i]]))
 }
-
-df <- subset(df, select = -c(HUQ010))
 
 sig_columns_score <- df_significance_testing(subset(df, select = -c(dep_cat)), 'dep_score', 'SEQN')
 sig_columns_cat <- df_significance_testing(subset(df, select = -c(dep_score)), 'dep_cat', 'SEQN')
@@ -132,6 +128,70 @@ s[, 3] <- s[, 3] - s[, 3]
 s
 plot(s, which=1:3, pch=1:3, xlab='logit', main=' ', xlim=range(s[,3:4]))
 
-# regression analysis
+# OLR analysis
+reg_df <<- data.frame(
+  HIQ210 = rep(c('Yes', 'No'), 5120),
+  HUQ090 = rep(c('Yes', 'No'), 2560, each=2),
+  MCQ160A = rep(c('Yes', 'No'), 1280, each=4),
+  MCQ160B = rep(c('Yes', 'No'), 640, each=8),
+  MCQ160D = rep(c('Yes', 'No'), 320, each=16),
+  MCQ160F = rep(c('Yes', 'No'), 160, each=32),
+  MCQ160M = rep(c('Yes', 'No'), 80, each=64),
+  MCQ160P = rep(c('Yes', 'No'), 40, each=128),
+  MCQ160L = rep(c('Yes', 'No'), 20, each=256),
+  MCQ550 = rep(c('Yes', 'No'), 10, each=512),
+  OSQ230 = rep(c('Yes', 'No'), 5, each=1024),
+  gen_health = rep(c("good","very_good","excellent","fair","poor"), each=1024)
+)
 
+reg_df_probs <- cbind(reg_df, predict(m, reg_df, type = "probs"))
 
+lreg_df <- melt(reg_df_probs, id.vars = c(
+  'HIQ210', 'HUQ090', 'MCQ160A', 'MCQ160B', 'MCQ160D', 'MCQ160F',
+  'MCQ160M', 'MCQ160P', 'MCQ160L', 'MCQ550', 'OSQ230', 'gen_health'
+  ), variable.name = "Level", value.name = "Probability", )
+lreg_df$gen_health <- recode(
+  lreg_df$gen_health, 
+    'excellent'=5,
+    'very_good'=4,
+    'good'=3,
+    'fair'=2,
+    'poor'=1
+  )
+
+graphing_stuff <- function(df, name='TOTAL') {
+  graph_df <- df %>% group_by(gen_health, Level) %>% summarise(Probability=mean(Probability))
+  ggplot(graph_df, aes(
+    x = gen_health, 
+    y = Probability, 
+    fill = Level,
+  )) + geom_bar(position = 'dodge', stat = 'identity') + labs(
+    title = glue('Depression Probability: {name}'),
+    x = 'Reported General Health Status',
+    y = 'Probability of Outcome (0 to 1)',
+    aes(name='Depression Categorical Level')
+  )
+  ggsave(glue('figs/{name}.png'), width = 11, height = 8.5)
+}
+
+graphing_stuff(lreg_df)
+
+filter_options <- c('HIQ210', 'HUQ090', 'MCQ160A', 'MCQ160B', 'MCQ160D', 'MCQ160F',
+                    'MCQ160M', 'MCQ160P', 'MCQ160L', 'MCQ550', 'OSQ230')
+
+for (i in filter_options) {
+  graph_df_filtered <- lreg_df[lreg_df[[i]]=='Yes',]
+  for (j in filter_options) {
+    if (j==i) {next}
+    graph_df_filtered <- graph_df_filtered[graph_df_filtered[[j]]=='No',]
+  }
+  print(graph_df_filtered)
+  graphing_stuff(graph_df_filtered, i)
+}
+
+graph_df_filtered <- lreg_df
+for (i in filter_options) {
+  graph_df_filtered <- graph_df_filtered[graph_df_filtered[[i]]=='No',]
+}
+print(graph_df_filtered)
+graphing_stuff(graph_df_filtered, 'NONE')
